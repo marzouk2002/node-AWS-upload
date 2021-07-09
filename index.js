fda8ec3f4b834e49b88b442a4090e0615363cf58
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-07-09 07:36:24 +0100

    upload to aws done

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express()
 const storage = multer.diskStorage({
     destination: './public/uploads',
     filename: function(req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+        cb(null, 'uploadedFile' + path.extname(file.originalname))
     }
 })
 
